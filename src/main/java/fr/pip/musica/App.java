4a621c8ad7f3873756f2ebba859ab382039ebf73
Philippe PETER <philippepeter@new-host.home>
2014-01-28 21:19:24 +0100

    First commit

---
+++ src/main/java/fr/pip/musica/App.java
@@ -0,0 +1,13 @@
+package fr.pip.musica;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
